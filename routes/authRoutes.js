dd9b5f0d0a58d340304adb35211a2da969fc0ef8
Madi <madelyn.e.helton@gmail.com>
2019-02-07 17:40:51 -0600

    adding auth check

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,3 +1,30 @@
-var models = require("../models");
-var passport = require("passport");
-var jwt = require('jsonwebtoken');
+let models = require("../models");
+let passport = require("passport");
+let jwt = require('jsonwebtoken');
+
+module.exports = function(app) {
+    app.post('/api/auth', function(request, response) {
+        passport.authenticate(
+            'local', 
+            {session: false}, 
+            function(error, user, info) {
+                if (error || !user) {
+                    return response.status(400).json({
+                        message: 'Something is not right',
+                        user   : user,
+                        error  : error
+                    });
+                }
+               request.login(user, {session: false}, (error) => {
+                   if (error) {
+                       response.send(error);
+                   }
+                   console.log("user: " + user.username);
+                   // generate a signed son web token with the contents of user object and return it in the response
+                   const token = jwt.sign(user.toJSON(), 'your_jwt_secret');
+                   response.json({user, token});
+                });
+            }
+        )(request, response);
+    });
+};
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
 
 var db = require("./models");
 
@@ -11,6 +15,28 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+passport.use(new localStorage(
+  {
+    usernameField: 'username',
+    passwordField: 'password'
+  },
+  function(username, password, cd) {
+    console.log(user);
+    if(!user || !user.validatePassword(password)) {
+      return cd(null, false, {message: 'Incorrect email or password.'})
+    }
+
+}
+));
+
 
 // Handlebars
 app.engine(
