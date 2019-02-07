37c4d7b2566c6a15c5115da7e6a2edc9889f5afc
Madi <madelyn.e.helton@gmail.com>
2019-02-06 18:15:02 -0600

    adding new packages

---
+++ models/user.js
@@ -0,0 +1,14 @@
+'use strict';
+let bcrypt = require('bcrypt');
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
