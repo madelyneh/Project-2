1b444c36c817f8978b9a9c47c2bb58ac94b6dd4b
Madi <madelyn.e.helton@gmail.com>
2019-02-17 07:32:01 -0600

    debugging after deploy

--- server.js
+++ server.js
@@ -7,8 +7,8 @@
 // =============================================================
 var express = require("express");
 var exphbs = require("express-handlebars");
-// var logger = require('morgan');
-// var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 var bodyParser = require("body-parser");
 
 var db = require("./models");
@@ -18,74 +18,92 @@ var db = require("./models");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-// var passport = require('passport');
-// var LocalStrategy = require('passport-local');
-// var JWTStrategy = require('passport-jwt').Strategy;
-// let ExtractJWT = require('passport-jwt').ExtractJwt;
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+var JWTStrategy = require('passport-jwt').Strategy;
+let ExtractJWT = require('passport-jwt').ExtractJwt;
 
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
-// app.use(logger('dev'));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
+app.use(cookieParser());
 
-// passport.use(new LocalStrategy(
-//   {
-//     usernameField: 'username',
-//     passwordField: 'password'
-//   },
-//   function(username, password, cd) {
-//     db.User.findOne({ where:{ username: username }}).then(
-//       function(user) {
-//         if (!user || !user.validatePassword(password)) {
-//           return cd(null, false, {message: 'Incorrect email or password.'});
-//         };
-//           return cd(null, user, {message: 'Logged in Successfully.'});
-//       }
-//     ).catch(error => {
-//       cd(error)
-//       throw error;
-//     });
-//   }
-// ));
+// This checks if the user is logged in or not. If they arent it will redirect to the login page
+app.use(passport.authenticate('jwt', { session: false }))
+app.use(function (req, res, next) {
+  console.log(req.url);
+  if (req.user || req.url === '/api/auth' || req.url === '/api/authors') {
+    console.log('on to the next one');
+    return next();
+  }
+  return res.redirect("/author-manager.html");
+});
 
-// passport.use(
-//   new JWTStrategy({
-//     jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-//     secretOrKey : 'your_jwt_secret'},
-//     function(jwtPayload, done) {
-//       //find current users information
-//       try {
-//         return done(null, jwtPayload)
-//       } catch (error) {
-//         done(error);
-//       }
-//     }
-//   )
-// );
+passport.use(new LocalStrategy(
+  {
+    usernameField: 'username',
+    passwordField: 'password'
+  },
+  function(username, password, done) {
+    console.log("Server 52: Username:" + username + " Password: " + password);
+    // TODO Make this into a reusable function that can then get called in the jwt call in the 'try' section to pull up the user.
+    db.Author.findOne({ where:{ username: username }}).then(
+      function(user) {
+        console.log("[server.js] Passport Local Strategy");
+        console.log("Server 56: " + user);
+        if (!user || !user.validatePassword(password)) {
+          console.log(`[server.js] couldn't find user`);
+          return done(null, false, {message: 'Incorrect email or password.'});
+        };
+        console.log('[server.js] Logged in');
+        return done(null, user, {message: 'Logged in Successfully.'});
+      }
+    ).catch(error => {
+      done(error);
+    });
+  }
+));
 
-// Static directory
-app.use(express.static("public"));
+passport.use(
+  new JWTStrategy({
+    jwtFromRequest: function(req) {
+      var token = null;
+      if (req && req.cookies)
+      {
+          token = req.cookies['token'];
+      }
+      return token;
+    },
+    secretOrKey : 'your_jwt_secret'},
+    function(jwtPayload, done) {
+      //find current users information
+      console.log('[server.js] JWT Strategy')
+      try {
+        // TODO Put the 'get user' function here to pull up the user's info
+        console.log('In the try');
+        return done(null, jwtPayload)
+      } catch (error) {
+        console.log(error);
+        done(error);
+      }
+    }
+  )
+);
 
-// Do we need???
 // Handlebars
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
-// Routes
-// let secureRoute = require("./routes/apiRoutes");
-// app.use('/api/examples', passport.authenticate('jwt', {session: false}), secureRoute);
-// require("./routes/htmlRoutes")(app);
-// require("./routes/authRoutes")(app);
 
-// Their new routes
+// Routes
 require("./routes/html-routes.js")(app);
 require("./routes/author-api-routes.js")(app);
 require("./routes/post-api-routes.js")(app);
@@ -95,7 +113,7 @@ var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
+if (process.env.NODE_ENV === "production") {
   syncOptions.force = false;
 }
 
