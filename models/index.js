c7f17bd3843de8915ca87f79dfee01613dcaa0b0
Madi <madelyn.e.helton@gmail.com>
2019-02-17 12:16:44 -0600

    change config file

--- models/index.js
+++ models/index.js
@@ -8,10 +8,8 @@ var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
-console.log('NODE ENVIRONMENT -- ', process.env.NODE_ENV);
-
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+if (env === "production") {
+  var sequelize = new Sequelize(process.env[config.uri]);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
